57a85ba5da092c83d1671171ed5be5035aa99f7c
Nicolas Abad <nicolasabad91@gmail.com>
2025-07-31 16:06:46 -0700

    moved to env file firebase keys

--- client/app.config.js
+++ client/app.config.js
@@ -35,7 +35,16 @@ export default {
     },
     extra: {
       API_URL: process.env.API_URL,
-      ADMIN_EMAIL: process.env.ADMIN_EMAIL
+      ADMIN_EMAIL: process.env.ADMIN_EMAIL,
+      firebase: {
+        apiKey: process.env.FIREBASE_API_KEY,
+        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+        appId: process.env.FIREBASE_APP_ID,
+        measurementId: process.env.FIREBASE_MEASUREMENT_ID
+      }
     }
   }
 };
--- client/firebase/config.js
+++ client/firebase/config.js
@@ -1,23 +1,15 @@
-// firebase/config.js
-
 import { initializeApp } from 'firebase/app';
 import {
   initializeAuth,
   getReactNativePersistence,
-  getAuth, getIdToken, signInWithEmailAndPassword
-
+  getAuth,
+  getIdToken,
+  signInWithEmailAndPassword
 } from 'firebase/auth';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import Constants from 'expo-constants';
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyArJWqCxkFVhyxGnlx1wKkYrPyrvSAreMo',
-  authDomain: 'silentauctionapp-6aef4.firebaseapp.com',
-  projectId: 'silentauctionapp-6aef4',
-  storageBucket: 'silentauctionapp-6aef4.appspot.com',
-  messagingSenderId: '777415470954',
-  appId: '1:777415470954:web:151940056ad2b37921ff13',
-  measurementId: 'G-KHFDTZ2C3Z',
-};
+const firebaseConfig = Constants.expoConfig.extra.firebase;
 
 const app = initializeApp(firebaseConfig);
 
@@ -30,5 +22,4 @@ try {
   auth = getAuth(app);
 }
 
-
-export { auth,signInWithEmailAndPassword, getIdToken }; // Export auth and other functions as needed
+export { auth, signInWithEmailAndPassword, getIdToken };
